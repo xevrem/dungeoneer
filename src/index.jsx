f9bf07c2aacbc8ed48d9ef8ab4378270ee610dae
Erika Jonell <erika.jonell@gmail.com>
2025-07-01 15:19:19 -0400

    cleanup, remove unneeded serviceworker

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-
 import { App } from "components/App";
 import { Game } from "Game";
-import registerServiceWorker from "./registerServiceWorker";
 
-/*
- * GAME CLASS
- */
 const MOUNT_NODE = document.getElementById("app");
 const root = createRoot(MOUNT_NODE);
 const game = new Game();
 root.render(<App game={game} />);
-
-// registerServiceWorker();
