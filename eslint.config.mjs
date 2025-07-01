8be0057b619932a5fcc32dd56883fdb74b8b174d
Erika Jonell <erika.jonell@gmail.com>
2025-07-01 14:20:58 -0400

    eslint and prettier configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,10 +2,25 @@ import js from "@eslint/js";
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
-
+import pluginPritter from 'eslint-plugin-prettier/recommended';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: { globals: globals.browser },
+  },
   pluginReact.configs.flat.recommended,
+  pluginPrettier,
+  {
+    rules: {
+      indent: ["warn", 2],
+      quotes: ["warn", "single"],
+      semi: ["error", "always"],
+    },
+  },
 ]);
